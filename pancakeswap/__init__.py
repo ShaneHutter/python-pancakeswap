cbd29b7ea9d51da3d8f004474b0041e98816b0e9
Shane Hutter <shane@hal9001.intentropycs.com>
2022-04-03 11:13:29 +1000

    Working on setup.py

--- pancakeswap/__init__.py
+++ pancakeswap/__init__.py
@@ -5,4 +5,29 @@ Description:
     Base package init
 """
 
-PKG_INFO = {}
+_package_name = "python-pancakeswap"
+__author__ = "Shane Hutter"
+_author_email = "shane@intentropycs.com"
+_author_url = "https://intentropycs.com/"
+__version__ = "0.0.0-1"
+_desc = "A python wrapper for the pancakeswap-info-api"
+_license = "GNU GPL v3"
+
+_pkg = [ "pancakeswap", ]
+
+PKG_INFO = {    
+    "name": _package_name,
+    "version":  __version__,
+    "author": __author__,
+    "author_email": _author_email,
+    "maintainer": __author__,
+    "maintainer_email": _author_email,
+    "url": _author_url,
+    "description": _desc,
+    "url": _author_url,
+    "download_url": _author_url,
+    "license": _license,
+    #"keywords": "",
+    #"platforms": "",
+    "packages": _pkg,
+    }
